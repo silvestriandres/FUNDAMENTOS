7e521401a28560cb32e187812735b466870af59f
@asilvestri_romedical.com <andres.silvestri@romedicalart.com>
2025-11-06 17:10:14 -0300

    S — Single Responsibility Principle (SRP)

---
+++ SOLID/[S]-Single Responsibility Principle/[S]-Single Responsibility Principle/CORRECTO/EmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace _S__Single_Responsibility_Principle.CORRECTO
+{
+    public class EmailSender
+    {
+        public void Send(string report) => Console.WriteLine("Email enviado");
+    }
+}
---
+++ SOLID/[S]-Single Responsibility Principle/[S]-Single Responsibility Principle/INCORRECTO/ReportService.cs
@@ -0,0 +1,9 @@
+﻿namespace _S__Single_Responsibility_Principle.INCORRECTO
+{
+    public class ReportService
+    {
+        public void GenerateReport() { Console.WriteLine("Reporte generado"); }
+        public void SaveToDatabase() { Console.WriteLine("Guardado en la DDBB"); }
+        public void SendEmail() { Console.WriteLine("Email enviado"); }
+    }
+}
